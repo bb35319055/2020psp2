dbb4b74d46d17923128dbda8f2b21d0cbd6b154d
bb35319055 <bb35319055@ms.nagasaki-u.ac.jp>
2020-07-09 23:21:06 +0900

    7/9 1回目

--- k02/k02.c
+++ k02/k02.c
@@ -1,13 +1,16 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#define _USE_MATH_DEFINES
 #include <math.h>
+
 
 extern double p_stdnorm(double z);
 
 int main(void)
 {
-    double val;
+    double ave_A=170.8,var_A=pow(5.43,2),ave_B=169.7,var_B=pow(5.5,2);
+    double val,y_A,y_B,f_A,f_B;
     char fname[FILENAME_MAX];
     char buf[256];
     FILE* fp;
@@ -26,12 +29,12 @@ int main(void)
 
     while(fgets(buf,sizeof(buf),fp) != NULL){
         sscanf(buf,"%lf",&val);
-
-
-    
-
-
-
+        y_A = (val - ave_A)/sqrt(var_A);
+        y_B = (val - ave_B)/sqrt(var_B);
+        f_A = p_stdnorm(y_A);
+        f_B = p_stdnorm(y_B);
+        L1 = L1 * f_A;
+        L2 = L2 * f_B;
     }
 
     if(fclose(fp) == EOF){
@@ -39,8 +42,8 @@ int main(void)
         exit(EXIT_FAILURE);
     }
 
-    printf("L_A: %f\n",max_val);
-    printf("L_B: %f\n",min_val);
+    printf("L_A: %f\n",L1);
+    printf("L_B: %f\n",L2);
 
     return 0;
 
